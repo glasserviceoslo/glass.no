4c1b8d21aec557587271fc86146885cd6a268464
Dovlat <dovlat93@gmail.com>
2022-10-13 22:53:18 +0200

    bumps package versions, configures node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
-
 import sanity from 'astro-sanity';
 
 // https://astro.build/config
@@ -19,5 +18,7 @@ export default defineConfig({
   server: {
     port: 3001,
   },
-  adapter: node(),
+  adapter: node({
+    mode: 'middleware',
+  }),
 });
