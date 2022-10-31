63bfe56f31a655e2a4cd18e95d475d610748f0c8
Dovlat <dovlat93@gmail.com>
2022-10-31 15:45:04 +0100

    adds footer, changes tailwind config back to cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,8 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-export default config;
