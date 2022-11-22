86d16c03f915a514e9af3e30dbd275a322988989
Dovlat <dovlat93@gmail.com>
2022-11-22 11:55:59 +0100

    adds analytics example script

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,20 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
 import sanity from 'astro-sanity';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
     sanity({
       projectId: 'csbn9wp4',
       dataset: 'glassno',
       apiVersion: '2021-10-21',
       useCdn: false,
+    }),
+    partytown({
+      config,
     }),
   ],
   output: 'server',
