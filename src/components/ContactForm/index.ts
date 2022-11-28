7a99b31652d305b31ff6868de153915844a7bef0
Dovlat <dovlat93@gmail.com>
2022-11-28 13:33:24 +0100

    adds form final

---
+++ src/components/ContactForm/index.ts
@@ -0,0 +1,23 @@
+import { createForm } from 'final-form';
+
+// const onSubmit = (values) => window.alert(JSON.stringify(values, undefined, 2));
+
+// const form = createForm({
+//   onSubmit,
+//   initialValues: {
+//     color: "#0000FF",
+//   },
+//   validate: (values) => {
+//     const errors = {};
+//     if (!values.firstName) {
+//       errors.firstName = "Required";
+//     }
+//     if (!values.lastName) {
+//       errors.lastName = "Required";
+//     }
+//     if (values.color === "#00FF00") {
+//       errors.color = "Gross! Not green! 🤮";
+//     }
+//     return errors;
+//   },
+// });
