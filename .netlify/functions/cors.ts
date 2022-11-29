dba2d9009d5ae9828ab0a3f0663cde1abd35d4ef
Dovlat <dovlat93@gmail.com>
2022-11-29 01:27:19 +0100

    fixes netlify function envs

--- .netlify/functions/cors.ts
+++ .netlify/functions/cors.ts
@@ -1,6 +1,6 @@
 import type { Handler } from '@netlify/functions';
 
-const corsOrigin = import.meta.env.DEV ? `http://localhost:3333` : `https://glass-no.sanity.studio`;
+const corsOrigin = process.env.NODE_ENV === 'development' ? `http://localhost:3333` : `https://glass-no.sanity.studio`;
 
 const handler: Handler = async (event, context) => {
   return {
