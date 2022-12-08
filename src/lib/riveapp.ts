811fb2e3d7dd373d516b88fd302a2c98dd5eb8ff
Dovlat <dovlat93@gmail.com>
2022-12-08 20:11:52 +0100

    fixes image component

--- src/lib/riveapp.ts
+++ src/lib/riveapp.ts
@@ -2,6 +2,6 @@ import { Rive } from '@rive-app/canvas';
 
 new Rive({
   src: '/assets/mobile.riv',
-  canvas: document.getElementById('canvas'),
+  canvas: document.getElementById('canvas') as HTMLCanvasElement,
   autoplay: true,
 });
