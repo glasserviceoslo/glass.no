ed8a53f355a0b4debe43e367a06d22afb9477554
Dovlat <dovlat93@gmail.com>
2023-01-04 19:09:48 +0100

    adds project schema, and booking page

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,6 +1,7 @@
 // schemas/schema.js
+import author from './author';
 import category from './category';
 import post from './post';
-import author from './author';
+import project from './project';
 
-export default [post, author, category];
+export default [post, author, category, project];
