6c87308edda024cec3225555a5fb30fb4eaee2f1
Dovlat <dovlat93@gmail.com>
2023-01-16 15:33:55 +0100

    fixes studio logo and styles

--- src/components/React/StudioLogo.tsx
+++ src/components/React/StudioLogo.tsx
@@ -1,10 +1,5 @@
 /** @jsxImportSource react */
 
-const Logo = () => (
-  <>
-    <img src="/assets/logo-dark.svg" className="mr-3 hidden h-10 w-auto dark:block md:h-12" alt="Glass.no Logo" />
-    <img src="/assets/logo-light.svg" className="mr-3 block h-10 w-auto dark:hidden md:h-12" alt="Glass.no Logo" />
-  </>
-);
+const Logo = () => <img src="/assets/logo-light.svg" className="w-automd:h-12 mr-3 block h-10" alt="Glass.no Logo" />;
 
 export default Logo;
