ed0defe296f80a05f0a0d593be82a9e9b003c379
Dovlat <dovlat93@gmail.com>
2023-01-17 16:50:22 +0100

    adds padding to studio logo

--- src/components/React/StudioLogo.tsx
+++ src/components/React/StudioLogo.tsx
@@ -1,5 +1,9 @@
 /** @jsxImportSource react */
 
-const Logo = () => <img src="/assets/logo-light.svg" className="w-automd:h-12 mr-3 block h-10" alt="Glass.no Logo" />;
+const Logo = () => (
+  <div className="px-4 py-2">
+    <img src="/assets/logo-light.svg" className="w-automd:h-12 mr-3 block h-10" alt="Glass.no Logo" />
+  </div>
+);
 
 export default Logo;
