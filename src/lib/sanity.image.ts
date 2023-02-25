2d6440f62b608746357778e652c727b7948e663e
Dovlat <dovlat93@gmail.com>
2023-02-25 16:56:41 +0100

    minor refactoring

--- src/lib/sanity.image.ts
+++ src/lib/sanity.image.ts
@@ -11,8 +11,9 @@ export const getSanityImageURL = (source: SanityImageSource) => builder.image(so
 
 export const removeExt = (filename: string) => filename.split('.').slice(0, -1).join('');
 
-export const parseImages = async (slug: string, excludedExt: string): Promise<ImgSources[]> =>
-  (await getPostBySlug(slug)).body
+export const parseImages = async (slug: string, excludedExt: string): Promise<ImgSources[]> => {
+  const posts = await getPostBySlug(slug);
+  return posts.body
     .filter((b: any) => b._type === 'image' && !b?.asset._id.endsWith(excludedExt))
     .map((image: Image) => ({
       default: getSanityImageURL(image.asset).quality(100).url(),
@@ -20,3 +21,4 @@ export const parseImages = async (slug: string, excludedExt: string): Promise<Im
       small: getSanityImageURL(image.asset).format('webp').width(650).url(),
       altText: image.asset.altText || removeExt(image.asset.originalFilename),
     }));
+};
