220ba5d338c06567016806ea85b91d55a88ab438
Dovlat <dovlat93@gmail.com>
2024-02-14 20:03:25 +0100

    cors wildcard

--- server.mjs
+++ server.mjs
@@ -7,12 +7,9 @@ const URL = process.env.URL || `http://localhost:${PORT}`;
 
 const app = express();
 
-const corsOptions = {
-  origin: ['https://glass.no', 'https://www.glass.no'],
-};
+const corsOptions = { origin: 'https://*.glass.no' };
 
 const base = '/';
-
 app.use(cors(corsOptions));
 app.use(base, express.static('dist/client/'));
 app.use(ssrHandler);
