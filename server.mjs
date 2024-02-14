295cfbfc3220d7dfe87548c5e0a33eef26a9c354
Dovlat <dovlat93@gmail.com>
2024-02-14 20:22:05 +0100

    cors 2

--- server.mjs
+++ server.mjs
@@ -1,16 +1,13 @@
 import express from 'express';
-import cors from 'cors';
 import { handler as ssrHandler } from './dist/server/entry.mjs';
 
 const PORT = process.env.PORT || 5001;
 const URL = process.env.URL || `http://localhost:${PORT}`;
 
 const app = express();
-
-const corsOptions = { origin: 'https://*.glass.no' };
-
 const base = '/';
-app.use(cors(corsOptions));
+
+app.use(base, cors());
 app.use(base, express.static('dist/client/'));
 app.use(ssrHandler);
 
