3bf8c06bd3008478bb5f071cf79bb50406edf244
Dovlat <dovlat93@gmail.com>
2024-02-20 23:58:25 +0100

    cors location

--- server.mjs
+++ server.mjs
@@ -8,12 +8,7 @@ const URL = process.env.URL || `http://localhost:${PORT}`;
 const app = express();
 const base = '/';
 
-app.use(base, cors());
-app.use(base, (_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 app.use(base, express.static('dist/client/'));
-app.use(ssrHandler);
+app.use(cors(), ssrHandler);
 
 app.listen(PORT, () => console.log(`Listening on ${URL}`));
