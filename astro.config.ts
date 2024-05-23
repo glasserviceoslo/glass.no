ee95e5a4b536f5ebd54d033b48ac2e9363a57a84
Dovlat Yakshibayev <dovlat93@gmail.com>
2024-05-23 02:02:51 +0200

    alias

--- astro.config.ts
+++ astro.config.ts
@@ -4,8 +4,17 @@ import partytown from '@astrojs/partytown';
 import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
 import node from '@astrojs/node';
+import { resolve } from 'node:path';
 
 export default defineConfig({
+  vite: {
+    resolve: {
+      alias: [
+        { find: '$', replacement: resolve(import.meta.dirname, './src') },
+        { find: '$tina', replacement: resolve(import.meta.dirname, './tina') },
+      ],
+    },
+  },
   site: 'https://www.glass.no',
   integrations: [
     tailwind({ applyBaseStyles: false }),
