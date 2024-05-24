cb8b148e2b03ecdcada0386c1feb1ea72924f8fd
Dovlat Yakshibayev <dovlat93@gmail.com>
2024-05-24 20:13:46 +0200

    collections structure

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -6,11 +6,11 @@ export default config({
   },
   collections: {
     pages: collection({
-      previewUrl: `/preview/start?branch={branch}&to=/pages/{slug}`,
+      previewUrl: "/preview/start?branch={branch}&to=/pages/{slug}",
       label: "Pages",
       slugField: "title",
       path: "src/content/pages/*",
-      format: { contentField: "body" },
+      format: { contentField: "content" },
       schema: {
         title: fields.slug({
           name: {
@@ -64,18 +64,18 @@ export default config({
           label: "Date",
           defaultValue: { kind: "today" },
         }),
-        body: fields.markdoc({
+        content: fields.markdoc({
           label: "Rich Text",
           extension: "md",
         }),
       },
     }),
     posts: collection({
-      previewUrl: `/preview/start?branch={branch}&to=/posts/{slug}`,
+      previewUrl: "/preview/start?branch={branch}&to=/posts/{slug}",
       label: "Posts",
       slugField: "title",
       path: "src/content/posts/*",
-      format: { contentField: "body" },
+      format: { contentField: "content" },
       schema: {
         title: fields.slug({
           name: {
@@ -117,8 +117,61 @@ export default config({
           label: "Date",
           defaultValue: { kind: "today" },
         }),
-        body: fields.markdoc({
-          label: "Body",
+        content: fields.markdoc({
+          label: "Rich Text",
+          extension: "md",
+        }),
+      },
+    }),
+    glassTypes: collection({
+      previewUrl: "/preview/start?branch={branch}&to=/glass-types/{slug}",
+      label: "Glass types",
+      slugField: "title",
+      path: "src/content/glass-types/*",
+      format: { contentField: "content" },
+      schema: {
+        title: fields.slug({
+          name: {
+            label: "Title",
+            description: "The title of the post",
+            validation: { isRequired: true },
+          },
+          slug: {
+            label: "SEO-friendly slug",
+            description: "This will define the file/folder name for this entry",
+          },
+        }),
+        // mainImage: fields.cloudImage({
+        //   url: fields.image({
+        //     label: "Main image",
+        //   }),
+        //   alt: fields.text({
+        //     label: "Alt text",
+        //   }),
+        // }),
+        mainImage: fields.image({
+          label: "Main image",
+          directory: "public/assets/uploads",
+          publicPath: "/assets/uploads/",
+        }),
+        description: fields.text({
+          label: "Description",
+          description: "Description for search engine optimization",
+        }),
+        seoKeyphrase: fields.text({
+          label: "Keyphrase",
+          description: "Keyphrase for search engine optimization",
+        }),
+        seoKeywords: fields.text({
+          label: "Keywords",
+          description: "Comma separated list of keywords",
+        }),
+        date: fields.date({
+          label: "Date",
+          defaultValue: { kind: "today" },
+        }),
+        content: fields.markdoc({
+          label: "Rich Text",
           extension: "md",
         }),
       },
