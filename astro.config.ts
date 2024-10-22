df89b17ef2c828d39b9967acb1dc90ec8e5d5371
Dovlat Yakshibayev <dovlat93@gmail.com>
2024-10-22 21:27:28 +0200

    sitemap integration

--- astro.config.ts
+++ astro.config.ts
@@ -6,10 +6,47 @@ import svelte from '@astrojs/svelte';
 import node from '@astrojs/node';
 import mdx from '@astrojs/mdx';
 import keystatic from '@keystatic/astro';
+import { join } from 'node:path';
+import { readdirSync, readFileSync } from 'node:fs';
+import matter from 'gray-matter';
+
+import sitemap from '@astrojs/sitemap';
+
+const contentDir = join(import.meta.dirname, 'src', 'content');
+const siteUrl = 'https://www.glass.no';
+
+function getRoutes(directory: string, prefix: string = '') {
+  return readdirSync(join(contentDir, directory))
+    .filter((file) => file.endsWith('.md'))
+    .map((file) => {
+      const content = readFileSync(join(contentDir, directory, file), 'utf-8');
+      const { data } = matter(content);
+      const slug = file.replace('.mdx', '');
+
+      if (data.navigationTitle) {
+        switch (directory) {
+          case 'pages':
+            return `${prefix}/${slug}`;
+          case 'posts':
+            return `${prefix}/posts/${slug}`;
+          case 'glasstypes':
+            return `${prefix}/glasstyper/${slug}`;
+          default:
+            return `${prefix}/${slug.toLowerCase().replace(/\s+/g, '-')}`;
+        }
+      }
+      return null;
+    })
+    .filter(Boolean);
+}
+
+const pageRoutes = getRoutes('pages', siteUrl);
+const postRoutes = getRoutes('posts', siteUrl);
+const glasstypeRoutes = getRoutes('glasstypes', siteUrl);
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.glass.no',
+  site: siteUrl,
   vite: {
     build: {
       rollupOptions: {
@@ -30,6 +67,12 @@ export default defineConfig({
     svelte(),
     mdx(),
     keystatic(),
+    sitemap({
+      customPages: [...pageRoutes, ...postRoutes, ...glasstypeRoutes].filter(Boolean) as string[],
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date('2024-10-22'),
+    }),
   ],
   output: 'hybrid',
   adapter: node({
